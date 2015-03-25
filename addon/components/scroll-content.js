93b44867429a82973ab46990d151a4d400078d50
Ben <benjamin.schoenburg@madvertise.com>
2015-03-25 14:38:15 +0100

    export config api

--- addon/components/scroll-content.js
+++ addon/components/scroll-content.js
@@ -5,29 +5,36 @@ export default Ember.Component.extend({
   layout: layout,
   classNames: 'scroll-content',
 
+  duration: null,
+  offset: null,
+
   didInsertElement: function() {
     this.$().css('white-space', 'nowrap');
     this.$().css('overflow', 'visible');
     this.$().css('float', 'left');
     var innerWidth = this.$().width();
-    this.$().css('white-space', 'initial');
     this.$().css('overflow', 'hidden');
     this.$().css('float', 'none');
     var outerWidth = this.$().width();
-    var offset = innerWidth - outerWidth;
+    if (Ember.typeOf(this.get('offset')) === 'null') {
+      this.set('offset', innerWidth - outerWidth);
+    }
+    if (Ember.typeOf(this.get('duration')) === 'null') {
+      this.set('duration', this.get('offset') * 10 )
+    }
 
     var _this = this;
     this.$().mouseover(function() {
       Ember.run.scheduleOnce('afterRender', this, function(){
         _this.$().stop();
-        _this.$().animate({scrollLeft: offset}, 3000, 'linear');
+        _this.$().animate({scrollLeft: _this.get('offset')}, _this.get('duration'), 'linear');
       });
     });
 
     this.$().mouseout(function() {
       Ember.run.scheduleOnce('afterRender', this, function(){
         _this.$().stop();
-        _this.$().animate({scrollLeft: 0}, 3000, 'linear');
+        _this.$().animate({scrollLeft: 0}, _this.get('duration'), 'linear');
       });
     });
   }
