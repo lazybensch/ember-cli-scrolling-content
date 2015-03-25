d719c40e3cda7435f647878b5d80d23f64d204a4
Ben <benjamin.schoenburg@madvertise.com>
2015-03-25 14:37:02 +0100

    basic scrolling

--- addon/components/scroll-content.js
+++ addon/components/scroll-content.js
@@ -4,17 +4,31 @@ import layout from '../templates/components/scroll-content';
 export default Ember.Component.extend({
   layout: layout,
   classNames: 'scroll-content',
-  attributeBindings: ['width'],
 
   didInsertElement: function() {
-    this.$().css('overflow', 'scroll')
+    this.$().css('white-space', 'nowrap');
+    this.$().css('overflow', 'visible');
+    this.$().css('float', 'left');
+    var innerWidth = this.$().width();
+    this.$().css('white-space', 'initial');
+    this.$().css('overflow', 'hidden');
+    this.$().css('float', 'none');
+    var outerWidth = this.$().width();
+    var offset = innerWidth - outerWidth;
 
+    var _this = this;
     this.$().mouseover(function() {
-      console.log('in');
+      Ember.run.scheduleOnce('afterRender', this, function(){
+        _this.$().stop();
+        _this.$().animate({scrollLeft: offset}, 3000, 'linear');
+      });
     });
 
     this.$().mouseout(function() {
-      console.log('out');
+      Ember.run.scheduleOnce('afterRender', this, function(){
+        _this.$().stop();
+        _this.$().animate({scrollLeft: 0}, 3000, 'linear');
+      });
     });
   }
 });
