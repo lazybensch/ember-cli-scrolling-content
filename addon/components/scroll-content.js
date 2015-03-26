768e9a7f139a429962f94d81470e25ec8d15fb2c
Ben <benjamin.schoenburg@madvertise.com>
2015-03-26 10:18:47 +0100

    refactor toward computed properties

--- addon/components/scroll-content.js
+++ addon/components/scroll-content.js
@@ -9,31 +9,39 @@ export default Ember.Component.extend({
   offset: null,
   speed: null,
 
-  mouseEnter: function(){
+  mouseEnter: function() {
     this.$().stop();
     var scrollRatio = (this.get('offset') - this.$().scrollLeft()) / this.get('offset');
     this.$().animate({scrollLeft: this.get('offset')}, this.get('duration') * scrollRatio, 'linear');
   },
 
-  mouseLeave: function(){
+  mouseLeave: function() {
     this.$().stop();
     var scrollRatio = this.$().scrollLeft() / this.get('offset');
     this.$().animate({scrollLeft: 0}, this.get('duration') * scrollRatio, 'linear');
   },
 
-  didInsertElement: function() {
-
-    this.$().css('display', 'block');
-    this.$().css('white-space', 'nowrap');
-    this.$().css('overflow', 'visible');
-    this.$().css('float', 'left');
+  innerWidth: function() {
+    this.$().css({ 'overflow': 'visible', 'float': 'left', });
     var innerWidth = this.$().width();
-    this.$().css('overflow', 'hidden');
-    this.$().css('float', 'none');
-    var outerWidth = this.$().width();
+    this.$().css({'overflow': 'hidden', 'float': 'none'});
+    return innerWidth;
+  }.property(),
+
+  outerWidth: function() {
+    return this.$().width();
+  }.property(),
+
+  didInsertElement: function() {
+    this.$().css({
+      'display': 'block',
+      'white-space': 'nowrap',
+      'overflow': 'hidden',
+      'float': 'none',
+    });
 
     if (Ember.typeOf(this.get('offset')) === 'null') {
-      this.set('offset', innerWidth - outerWidth);
+      this.set('offset', this.get('innerWidth') - this.get('outerWidth'));
     }
     if (Ember.typeOf(this.get('duration')) === 'null') {
       this.set('duration', this.get('offset') * 10 );
