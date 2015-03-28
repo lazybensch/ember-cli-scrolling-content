7a7eaa8f36d80b7f0ffc54f1efe93899672c4c06
Ben <benjamin.schoenburg@madvertise.com>
2015-03-28 08:52:33 +0100

    add advanced guides

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -3,14 +3,27 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   value: 'I am too short :(',
 
-  //BEGIN-SNIPPET actions
   actions: {
+    //BEGIN-SNIPPET foo-bar
     foo: function() {
       window.alert('finished scrolling');
     },
     bar: function() {
       window.alert('scrolled all the way back');
-    }
+    },
+    //END-SNIPPET
+    //BEGIN-SNIPPET toggle-scrolling
+    toggleScrolling: function() {
+      this.toggleProperty('scrollForward');
+    },
+    //END-SNIPPET
+    //BEGIN-SNIPPET advanced-actions
+    startScrolling: function() {
+      this.set('started', true);
+    },
+    goBack: function() {
+      this.set('started', false);
+    },
+    //END-SNIPPET
   }
-  //END-SNIPPET
 });
