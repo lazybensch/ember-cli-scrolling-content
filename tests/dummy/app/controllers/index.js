99e55500b5cfc7209df489e10a28a5aee6285468
Ben <benjamin.schoenburg@madvertise.com>
2015-03-28 08:52:05 +0100

    add guide section for scroll events

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -1,5 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  value: 'I am too short :('
+  value: 'I am too short :(',
+
+  //BEGIN-SNIPPET actions
+  actions: {
+    foo: function() {
+      window.alert('finished scrolling');
+    },
+    bar: function() {
+      window.alert('scrolled all the way back');
+    }
+  }
+  //END-SNIPPET
 });
